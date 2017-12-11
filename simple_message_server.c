bb7d25d7b8dc6fb73800fccfb01b247c38c8ab03
salko.nuhanovic <salko.nuhanovic@accenture.co>
2017-12-11 22:25:01 +0100

    adapt comments for server

--- simple_message_server.c
+++ simple_message_server.c
@@ -14,12 +14,12 @@
  /*
  * -------------------------------------------------------------- includes --
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <errno.h> 
+#include <errno.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <arpa/inet.h>
@@ -33,9 +33,9 @@
  */
 
  /**
- * Maxvalue for queue
+ * How many pending connections queue will hold
  */
- 
+
  #define BACKLOG 10
 
 /*
@@ -66,12 +66,12 @@ static const char * const business_logic = "simple_message_server_logic";
  * @param port Portnumber
  * @return static int socketfiledescriptor
  */
- 
+
 static int createSocket(char *port);
 
 /**
  * @brief handles the accept() SysCall and the forking
- * @param socketfd
+ * @param int socketfd
  */
 
 static void execServer(int socketfd);
@@ -79,11 +79,11 @@ static void execServer(int socketfd);
 /**
  * @brief handles the Child Processes
  * @details prevents the occurrence of Zombie-Processes
- * @param s signal
+ * @param int s signal
  */
- 
-static void handleChildSignals(int s);
- 
+
+static void sigchld_handler(int s);
+
  /**
  * @brief prints a usage message to stdout or stderr
  * @details prints the usage message and then calls exit()
@@ -92,7 +92,7 @@ static void handleChildSignals(int s);
  * @param progName prints the Programname to stdout, stderr
  * @param exitCode the exitcode used for exit
  */
- 
+
 static void printUsage(FILE *stream, const char *progName, int exitCode);
 
 /**
@@ -112,27 +112,27 @@ int main(int argc,char *argv[]) {
 	int args;
 	int portNumber =0;
 	char * endptr = NULL;
-	
+
 	if(argc != 3){ //checks the argument-count
 		printUsage(stderr, argv[0], EXIT_FAILURE);
 	}
-	
+
 	while ((args = getopt(argc, argv, "p:h")) != -1) //getopt parses the commandline arguments
 	{
 		switch (args)
 		{
-			case 'p': 
-				port = optarg; //return value of getopt, Assignment to port
+			case 'p':
+				port = optarg; //optarg holds the portnumber-string
 				if(port == NULL)
 				{
 					printUsage(stderr, argv[0], EXIT_FAILURE);
 				}
 				errno = 0;
-				portNumber = strtol(port, &endptr, 10);//converts the initial part of port into a int, according base 10
+				portNumber = strtol(port, &endptr, 10);//converts the port-string into a int, according base 10 (decimal)
 				if(errno != 0) {
 					printUsage(stderr, argv[0], EXIT_FAILURE);
 				}
-				if(*endptr != 0) {
+				if(*endptr != '\0') {
 					printUsage(stderr, argv[0], EXIT_FAILURE);
 				}
 				if (portNumber < 0 || portNumber > 65535) { // checks the portrange
@@ -142,14 +142,14 @@ int main(int argc,char *argv[]) {
 			case 'h':
 				printUsage(stdout, argv[0], EXIT_SUCCESS);
 				break;
-			case '?':
+			case '?': //any input differing from 'p' and 'h'
 			default:
 				printUsage(stderr, argv[0], EXIT_FAILURE);
 				break;
 		}
 	}
 
-	socketfd = createSocket(port); 
+	socketfd = createSocket(port);
 
 	execServer(socketfd);
 
@@ -160,16 +160,16 @@ static int createSocket(char *port) {
     struct addrinfo hints;
     struct addrinfo *addrInfo, *p;
     struct sigaction signalAction;
-	int true=1;
+	int yes=1;
 	int socketfd = -1;
 	int rv;
 
     memset(&hints, 0, sizeof hints); //Initializing
-	hints.ai_family = AF_INET; //ipv4
-	hints.ai_socktype = SOCK_STREAM; //TCP stream sockets
-	hints.ai_flags = AI_PASSIVE;//fills in my IP
+	hints.ai_family = AF_INET; //Socket communicates with ipv4 Addresse
+	hints.ai_socktype = SOCK_STREAM; //provides TCP
+	hints.ai_flags = AI_PASSIVE;//Resolves the local Address and Port
 
-	//returns one or more addrinfo structures, each of which contains an Internet address 
+	//returns one or more addrinfo structures, each of which contains an Internet address
 	//that can be specified in a call to bind(2) or connect(2).
 	if ((rv = getaddrinfo(NULL, port, &hints, &addrInfo)) != 0){
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
@@ -182,8 +182,8 @@ static int createSocket(char *port) {
             perror("server: socket");
             continue;
         }
-		//set socket options, SOL_SOCKET = set options at the socket level, SO_REUSEADDR = allows other sockets to bind
-		if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &true, sizeof(int)) == -1) {
+		//set socket options, SOL_SOCKET = set options at the socket level -> SO_REUSEADDR = allows other sockets to bind
+		if (setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
 			close(socketfd);
 			freeaddrinfo(addrInfo);
 			perror("setsockopt");
@@ -197,7 +197,7 @@ static int createSocket(char *port) {
         }
         break;
     }
-	
+
 	if (p == NULL)  {
        fprintf(stderr, "server: failed to bind\n");
        freeaddrinfo(addrInfo);
@@ -206,41 +206,41 @@ static int createSocket(char *port) {
 	freeaddrinfo(addrInfo);
 
 	/* listen */
-	if (listen(socketfd, BACKLOG) == -1) { 
+	if (listen(socketfd, BACKLOG) == -1) {
 		perror("Error listening.");
-		close(socketfd);//Fehlerhandling
+		close(socketfd);
 		exit(EXIT_FAILURE);
 	}
 
 	/* set SignalAction for Child  - prevents Zombie-Processes*/
-	signalAction.sa_handler = handleChildSignals;
+	signalAction.sa_handler = sigchld_handler;
 	sigemptyset(&signalAction.sa_mask);
 	signalAction.sa_flags = SA_RESTART;
-	if (sigaction(SIGCHLD, &signalAction, NULL) == -1) {
+	if (sigaction(SIGCHLD, &signalAction, NULL) == -1) { //SIGCHILD provides information about the child
 		perror("Error performing sigaction.");
-		close(socketfd); 
+		close(socketfd);
 		exit(EXIT_FAILURE);
 	}
 
     return socketfd;
 }
 
-static void execServer(int socketfd) { 
-	struct sockaddr_storage cliSockAddr; 
-	socklen_t cliSockSize;
+static void execServer(int socketfd) {
+	struct sockaddr_storage clientSockAddr;
+	socklen_t clientSockSize;
 	int newSocketfd = -1;
 
 	while(1) { //runs the accept()and fork() SysCall in a loop
-		cliSockSize = sizeof(cliSockAddr);
+		clientSockSize = sizeof(clientSockAddr);
 
-		newSocketfd = accept(socketfd, (struct sockaddr *) &cliSockAddr, &cliSockSize); //accept the connection from the client
+		newSocketfd = accept(socketfd, (struct sockaddr *) &clientSockAddr, &clientSockSize); //accept the connection from the client
 		if (newSocketfd == -1) {
 			perror("Error creating new socket.");
 			close(socketfd);
 			exit(EXIT_FAILURE);
 		}
 
-		/* FORKING */ 
+		/* FORKING */
 		switch(fork())
 		{
 			case -1: //Failure
@@ -265,11 +265,11 @@ static void execServer(int socketfd) {
 			default: //Parent-Process
 				close(newSocketfd);
 		}
-		
+
 	}
 }
 
-static void handleChildSignals(int s){
+static void sigchld_handler(int s){
 	int saved_errno = errno;
 	s = s;
     while(waitpid(-1, NULL, WNOHANG) > 0); //wait for the termination of all child-processes
@@ -281,7 +281,7 @@ static void printUsage(FILE *stream, const char *programName, int exitCode) {
 	fprintf(stream, "options:\n");
 	fprintf(stream, "\t-p, --port <port>\n");
 	fprintf(stream, "\t-h, --help\n");
-	
+
 	exit(exitCode);
 }
 
